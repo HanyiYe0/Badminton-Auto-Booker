75d7c5007d189e7edd3456e4339e64f21f11ef51
HanyiYe0 <hanyiye2@gmail.com>
2025-03-27 10:54:28 -0400

    internal date formatting implemented

--- scripts/content.js
+++ scripts/content.js
@@ -2,13 +2,17 @@ chrome.storage.sync.get(['sportType', 'date', 'startTime', 'endTime'], (data) =>
     wantedStartTime = data.startTime;
     wantedEndTime = data.endTime;
     activity = data.sportType;
-    dateWanted = data.date;
-
+    dateWanted = formatDate(data.date);
+    isDate = false;
+    console.log(dateWanted);
     // Find all date rows
     const table = document.getElementById("classes");
     const rows = table.getElementsByTagName("tr");
     for (let i = 0; i < rows.length; i++) {
-        console.log(rows[i].textContent);
+        // check if the date is correct
+        if (rows[i].classList.contains('bm-marker-row')) {
+            console.log(rows[i].textContent);
+        };
     };
 });
 
@@ -29,6 +33,18 @@ function formatDate(date) {
         '11': 'Nov',
         '12': 'Dec'
     };
+
     let output = ""
+    // Check last index then convert into string
+    if (words[2].charAt(words[2].length-1) === '1') {
+        output = numToLetter[words[1]] + " " + Number(words[2]).toString() + "st, " + words[0];
+    } else if (words[2].charAt(words[2].length-1) === '2') {
+        output = numToLetter[words[1]] + " " + Number(words[2]).toString() + "nd, " + words[0];
+    } else if (words[2].charAt(words[2].length-1) === '3') {
+        output = numToLetter[words[1]] + " " + Number(words[2]).toString() + "rd, " + words[0];
+    } else {
+        output = numToLetter[words[1]] + " " + Number(words[2]).toString() + "th, " + words[0];
+    }
+    return output
 }
 
